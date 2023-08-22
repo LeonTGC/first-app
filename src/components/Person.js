2bacf885250c3ab26830b423b815739ecae3013e
LeonTGC <leon.cooper@gceducationandskills.ac.uk>
2023-08-22 10:36:31 +0100

    added person file

---
+++ src/components/Person.js
@@ -0,0 +1,11 @@
+const Person = ({ name, age }) => {
+    return (
+        <div>
+            <h2>my name is {name}</h2>
+            <p>i am {age}</p>
+        </div>
+    )
+}
+
+export default Person
+
